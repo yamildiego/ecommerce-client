3d91d41c0fed3f9a3ee695ad5391f0d89952c135
yamildiego <yamildiego@gmail.com>
2023-01-19 12:57:45 +1100

    refactorize

--- e.client/src/Constants/Urls.js
+++
@@ -1,9 +0,0 @@
-export const baseUrl =
-  process.env.REACT_APP_PRODUCTION === "1" ? process.env.REACT_APP_BASE_URL_PRODUCTION : process.env.REACT_APP_BASE_URL_DEVELOPMENT;
-
-//ecommerce
-export const loadProducts = baseUrl + "products/";
-export const getProduct = baseUrl + "products/getProduct";
-
-//delivery
-export const reviewAndPay = baseUrl + "create-checkout-session";
---
+++ src/Constants/Urls.js
@@ -0,0 +1,8 @@
+export const baseUrl = process.env.BASE_URL;
+
+//ecommerce
+export const loadProducts = baseUrl + "/products/";
+export const getProduct = baseUrl + "/products/getProduct";
+
+//delivery
+export const reviewAndPay = baseUrl + "/create-checkout-session";
